ecf0b82139a588650a79aa429d96591adffc2c11
FlorianHasuik <fhasuik@gmail.com>
2014-10-21 01:21:21 +0200

    Demo route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :documents
   
-
+  root :to => "home#index"
   #C'est le test
 
   # The priority is based upon order of creation: first created -> highest priority.
